98d0b588547ac18cb63628fe2f098d8059ed7770
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-14 17:38:47 -0800

    updated command.js
    Removed comments, fixed query 3 command in 'manage the database'

--- commands.js
+++ commands.js
@@ -3,18 +3,13 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
-/*
     use jaskis
-*/
 
 // 2. Create a collection called bounties
-/*
     db.createCollection('bounties')
-*/
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-/*
     db.bounties.insertOne({
         name: "Thanoceros",
         species: "Rhinoceros",
@@ -24,15 +19,11 @@
         reward: 10000,
         captured: false
     })
-*/
 
 // 2. Query for all bounties in the bounties collection
-/*
     db.bounties.find()
-*/
 
 // 3. Insert many bounties at once using the given objects
-/*
     db.bounties.insertMany([
     {
         "name": "Lokinkajou",
@@ -89,69 +80,44 @@
         "captured": false
     }
     ])
-*/
 
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-/*
     db.bounties.find({location: {$eq: 'Grasslands'}})
-*/
 
 // 2. Query for all bounties with a reward worth 10000 or more
-/*
     db.bounties.find({reward: {$gt: 10000}})
-*/
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
-/*
-    db.bounties.find({client: {$ne: 'Red wolf'}})
-*/
+    db.bounties.find({}, {client: 0})
 
 // 4. Query for a Groundhog in the Woodlands
-/*
     db.bounties.find({$and: [{species: {$eq: 'Groundhog'}}, {location: {$eq: 'Woodlands'}}]})
-*/
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-/*
     db.bounties.updateOne({name: {$eq: 'Polarwind'}}, {$set: {reward: 10000}})
-*/
 
 // 2. Remove Lokinkajou
-/*
     db.bounties.deleteOne({name: {$eq: 'Lokinkajou'}})
-*/
 
 // 3. Delete all bounties sent by Songbird
-/*
     db.bounties.deleteMany({client: {$eq: 'Songbird'}})
-*/
 
 // 4. Update all captured statuses to true
-/*
     db.bounties.updateMany({}, {$set: {captured: true}})
-*/
 
 // BONUS
 //Queries
 // 1. Find all Scavengers who joined the team after December 31, 2011
-/*
     db.scavengers.find({joined: {$gte: ISODate("2011-12-31")}})
-*/
 
 // 2. Find all Scavengers who helped catch Thanoceros.
-/*
     db.scavengers.find({"captured.name": {$eq: "Thanoceros"}})
-*/
 
 // 3. Find all Scavengers who helped catch an animal bounty with a reward greater than $11000.
-/*
     db.scavengers.find({"captured.name": {$eq: "Thanoceros"}})
-*/
 
 // 4. Find all Scavengers that don't have a weapon.
-/*
     db.scavengers.find({weapon: {$eq: null}})
-*/
